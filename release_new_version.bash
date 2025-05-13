940e20de7b09b4c1dfa5faa45b09443a65d18f6b
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2025-05-13 15:30:11 +0200

    DM-FlexiLogXML 2.2.2-STABLE
    Signed-off-by: Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>

--- release_new_version.bash
+++ release_new_version.bash
@@ -1,1 +1,1 @@
-git add . && git commit -s -m "Utils $1" && git tag -s $1 -m "Utils $1" && git push && git push --tags && git checkout main && git pull && git merge development && git push && git push --tags && git checkout development
+git add . && git commit -s -m "DM-FlexiLogXML $1" && git tag -s $1 -m "DM-FlexiLogXML $1" && git push && git push --tags
